1f391f765aa4fa11949422d43218da2a33a83b84
Elpi-code <70661885+Elpi-code@users.noreply.github.com>
2020-12-19 22:14:11 -0500

    Update database.php

--- database.php
+++ database.php
@@ -16,6 +16,7 @@
 
         //Connection to database
         $conn = mysqli_connect($servername, $username, $password, $dbname);
+        
         if (!$conn) {
             die("Connection failed: " . mysqli_connect_error());
         }

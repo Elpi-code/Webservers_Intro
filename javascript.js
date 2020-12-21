5aa5cf5162f80cc11aa2ebda196c1408732d712c
Elpi-code <70661885+Elpi-code@users.noreply.github.com>
2020-12-21 15:24:52 -0500

    troubleshoot Java again

--- javascript.js
+++ javascript.js
@@ -1,13 +1,11 @@
 //Check if only letters are entered
 function justLetters(inputtxt){
     var letters = /^[A-Za-z]+$/;
-    var text;
     if(inputtxt.value.match(letters))
     {
-        text = "That is indeed a good choice!";
+        "That is indeed a good choice!";
     }
     else{
         alert("An animal name can only contain letters");
     }
-    document.getElementById("demo").innerHTML = text;
 }

6889eb8ade5cd30ef3697ada005b46be9f3883b8
Elpi-code <70661885+Elpi-code@users.noreply.github.com>
2020-12-22 13:51:28 -0500

    Update birds.php

--- birds.php
+++ birds.php
@@ -12,11 +12,11 @@
         <div>Here are some of them:</div>
         <ul>
             <li> <span style="color:dodgerblue">Falcon</span> </li>
-            <img src="Images/peregrinefalcon.jpg" alt="Peregrine falcon" />
+            <img src="Images/peregrinefalcon.jpg" alt="Peregrine falcon" width="290" height="430"/>
             <li><span style="color:dodgerblue">Pileated Woodpecker</span></li>
-            <img src="Images/pileatedwoodpecker.jpg" alt="Pileated Woodpecker" />
+            <img src="Images/pileatedwoodpecker.jpg" alt="Pileated Woodpecker" width="290" height="441" />
             <li><span style="color:dodgerblue">Cerulean Warbler</span></li>
-            <img src="Images/warbler.jpg" alt="A warbler" />
+            <img src="Images/warbler.jpg" alt="A warbler" width="290" height="266"/>
         </ul>
 
         <!--Get user data and sanitize it with JavaScript function-->

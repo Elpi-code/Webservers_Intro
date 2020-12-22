c79d4f0a94cef28244889e81f6d1800cdb3019e1
Elpi-code <70661885+Elpi-code@users.noreply.github.com>
2020-12-22 13:41:26 -0500

    Update amphibians.php

--- amphibians.php
+++ amphibians.php
@@ -20,9 +20,9 @@
         <!-- Show pictures of amphibians-->
         <ul>
             <li><span style="color:dodgerblue;"> Salamander</span> </li>
-            <img src="Images/salamander.jpg" alt="Salamander" width="512" height="283" />
+            <img src="Images/salamander.jpg" alt="Salamander" width="512" height="283" /></br>
             <li><span style="color:dodgerblue;"> Wood Frog</span> </li>
-            <img src="Images/woodfrog.jpg" alt="Wood Frog" width="512" height="384"/>
+            <img src="Images/woodfrog.jpg" alt="Wood Frog" width="512" height="384"/></br>
             <li><span style="color:dodgerblue;"> Garter Snake</span> </li>
             <img src="Images/gartersnake.png" alt="Garter Snake" width="512" height="324" />
         </ul>

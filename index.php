bad1b0a6f0e585073b19e917e5f181a816c94c29
Elpi-code <70661885+Elpi-code@users.noreply.github.com>
2020-12-22 14:43:15 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,29 +1,31 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>Introduction page to the website</title>
+    <head>
+        <title>Introduction page to the website</title>
 
-    <!--Call the Javascript functions-->
-    <script src="javascript.js"></script>
-</head>
+        <!--Call the Javascript functions-->
+        <script src="javascript.js"></script>
+    </head>
     <body>
     
-    <img src="Images/mont-st-hilaire.jpg" id="mshimage" width="500" height="333" 
-        onclick="changeImg()" value="Change"/>
+        <form name = "form2">
+        <img src="Images/mont-st-hilaire.jpg" id="mshimage" width="500" height="333" 
+            value="change" onclick="changeImg(document.form2.change)" />
+        </form>
 
-    <!--<img src="Images/mshlandscape" alt="Landscape" width="500" height="333"/>
-    <img src="Images/mont-st-hilaire.jpg" alt="Mont-Saint-Hilaire" />
-    <img src="Images/mshtopview" alt="Topview" width="500" height="333"/>
-    --></br>
+        <!--<img src="Images/mshlandscape" alt="Landscape" width="500" height="333"/>
+        <img src="Images/mont-st-hilaire.jpg" alt="Mont-Saint-Hilaire" />
+        <img src="Images/mshtopview" alt="Topview" width="500" height="333"/>
+        --></br>
 
-    <h1>Animals in the Mont-Saint-Hilaire region</h1>
-    <p>There are many different animals found around Mont-Saint-Hilaire. </br>
-        Around 800 species of butterflies, 8 species of frogs, 4 species of salamanders, 2 species of snakes, </br>
-        200 species of birds and many more live in the mountain.</p>
+        <h1>Animals in the Mont-Saint-Hilaire region</h1>
+        <p>There are many different animals found around Mont-Saint-Hilaire. </br>
+            Around 800 species of butterflies, 8 species of frogs, 4 species of salamanders, 2 species of snakes, </br>
+            200 species of birds and many more live in the mountain.</p>
     
-    <!--References to other pages in the website-->
-    <p>If you are interested in learning about some birds that nest on the mountain, click <a href="birds.php"> here</a>.</br>
-        If you prefer learning about some amphibians that reside around the mountain, click <a href="amphibians.php"> here</a>.</p>
+        <!--References to other pages in the website-->
+        <p>If you are interested in learning about some birds that nest on the mountain, click <a href="birds.php"> here</a>.</br>
+            If you prefer learning about some amphibians that reside around the mountain, click <a href="amphibians.php"> here</a>.</p>
 
     </body>
 </html>

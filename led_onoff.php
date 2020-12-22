8f00faa5c747b472e187705e5af52039c9a0a6e2
Elpi-code <70661885+Elpi-code@users.noreply.github.com>
2020-12-22 16:24:47 -0500

    Update led_onoff.php

--- led_onoff.php
+++ led_onoff.php
@@ -7,30 +7,12 @@
     <body>
         <h1>Toggle LED</h1>
 
-        <p>This page is used to turn an LED ON or OFF by cliking on the button below.</p>
+        <p>This page is used to turn an LED ON or OFF by cliking on the button below.</p></br>
         <form method="post">
             <input type="submit" name = "click" value="Click me"/></br>
         </form>
+        
         <?php
-            function toggle(){
-                //Get value of GPIO pin 0
-                $opt = `gpio read 0`;
-
-                //ON if pin is low
-                if(($opt = `gpio read 0`) == 0){
-                    $opt=  `gpio write gpio.0 1`;
-                    echo "<div>$opt</div>";
-                    echo "</br>Your LED should be ON!</br>";
-                }
-
-                //OFF if pin is high
-                else if (($opt = `gpio read 0`) == 1){
-                    $opt = `gpio write gpio.0 0`;
-                    echo "<div>$opt</div>";
-                    echo "</br>Your LED should be OFF now!</br>";
-                }
-            }
-
             //Execute the function when the form above has been submitted
             if(array_key_exists('click',$_POST)){
                 toggle();
@@ -43,6 +25,25 @@
             }
             else{
                 echo "</br>The switch is higher than ever!</br>";
+            }
+
+            function toggle(){
+                //Get value of GPIO pin 0 (the 3.3V)
+                $opt = `gpio read 0`;
+
+                //Toggle it ON if pin is low
+                if(($opt = `gpio read 0`) == 0){
+                    $opt=  `gpio write gpio.0 1`;
+                    echo "<div>$opt</div>";
+                    echo "</br>Your LED should be ON!</br>";
+                }
+
+                //Toggle it OFF if pin is high
+                else if (($opt = `gpio read 0`) == 1){
+                    $opt = `gpio write gpio.0 0`;
+                    echo "<div>$opt</div>";
+                    echo "</br>Your LED should be OFF now!</br>";
+                }
             }
         ?>
     </body>

ef0535d17cf61f17448dae68156cb59211dbef77
Elpi-code <70661885+Elpi-code@users.noreply.github.com>
2020-12-21 19:45:58 -0500

    Creation of the index.php intro page

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Introduction page to the website</title>
+</head>
+    <body>
+    
+    <img src="Images/mshlandscape" alt="Landscape" width="500" height="333">
+    <img src="Images/mont-saint-hilaire.jpg" alt="Mont-Saint-Hilaire" >
+    <img src="Images/mshtopview" alt="Topview" width="500" height="333">
+    <h1>Animals in the Mont-Saint-Hilaire region</h1>
+    <p>There are many different species of birds found around the mountain.</p>    
+
+    </body>
+</html>
